724755a633769d79662b95546046578a162725ac
Gerold Hintz <gehint@microsoft.com>
2020-04-02 17:58:45 +0200

    doc string

--- data_processing/run_all_processing.py
+++ data_processing/run_all_processing.py
@@ -1,3 +1,7 @@
+"""
+Downloads (on-demand) a range of dump files and processes them according to filters and processors specified below.
+"""
+
 import logging
 import argparse
 import urllib
@@ -13,21 +17,19 @@ from wiki_dump_download import existFile, get_dump_task
 from generic_extractor import process
 
 from custom_filters import *
-from custom_extractors import NDJsonExtractor
+from custom_extractors import *
 
 def download_on_demand(url, dump_file, data_path, compress_type):
     if not existFile(data_path, dump_file, compress_type):
         urllib.request.urlretrieve(url, dump_file)
     else:
         logging.debug("File Exists, Skip: " + url)
-
     return dump_file
 
 
-
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--index', type=int, help='the index of the file to download')
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('--index', type=int, help='the index of the file (within the dump status file) to download and process')
     #parser.add_argument('--start', type=int, default=1, help='the first file to download [default: 0]')
     #parser.add_argument('--end', type=int, default=-1, help='the last file to download [default: -1]')
     parser.add_argument('--dumpstatus_path', type=str, default='./data/raw/dumpstatus.json')
@@ -56,6 +58,10 @@ if __name__ == "__main__":
         ExtractLeftRightContext(5, 5)
     ]
 
+    ## chose extractor here, determines how each instance is serialized to output_stream
+    extractor = TsvExtractor(["comment"])
+    extractor = NDJsonExtractor()
+
     def open_azure_input_stream():
         raise NotImplementedError
 
@@ -72,7 +78,8 @@ if __name__ == "__main__":
     wiki_input_stream = open_azure_input_stream() if args.azure else bz2.open(dump_file, "rt", encoding='utf-8')
     json_output_stream = io.StringIO() if args.azure else open(output_file, "w", buffering=1, encoding='utf-8')
 
-    process(1, wiki_input_stream, json_output_stream, 1, 10, ctx_window=5, extractor=NDJsonExtractor(), filters=filters_and_processors)
+
+    process(1, wiki_input_stream, json_output_stream, 1, 10, ctx_window=5, extractor=extractor, filters=filters_and_processors)
     
     wiki_input_stream.close()
     if not args.azure:
